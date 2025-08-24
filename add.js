203dd892dc2a2d74b95f9ea0f9439e2689dd0bae
Jehad <161898237+Jehadov@users.noreply.github.com>
2025-08-24 23:50:24 +0300

    Update add.js

--- add.js
+++ add.js
@@ -1,7 +1,12 @@
-function addAndSubtract(amount, balance) {
-    return {
-        added: amount + balance,
-        subtracted: balance - amount
-    };
+function addBalance(balance, new_amount_to_add) {
+  const b = Number(balance) || 0;
+  const add = Number(new_amount_to_add) || 0;
+  return b + add;
 }
-module.exports = { addAndSubtract };
+
+function subtractBalance(balance, new_amount_to_subtract) {
+  const b = Number(balance) || 0;
+  const sub = Number(new_amount_to_subtract) || 0;
+  return b - sub;
+}
+
